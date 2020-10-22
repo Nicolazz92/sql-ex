bb55250b1a16705db18ea917b820b300c9c29cb5
sbt-velikokhatko-nn <NNVelikokhatko.SBT@sberbank.ru>
2020-10-22 16:46:24 +0300

    8

--- sql_learn_pg/8.sql
+++ sql_learn_pg/8.sql
@@ -1,12 +1,3 @@
--- select maker
--- from product
---          join pc on product.model = pc.model
---     except
--- select maker
--- from product
---          join laptop on product.model = laptop.model
-
-
 select distinct product.maker from product where product.type = 'PC'
 except
 select distinct product.maker from product where product.type = 'Laptop'
