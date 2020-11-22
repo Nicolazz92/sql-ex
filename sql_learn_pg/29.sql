4234698cfcc127a28de45b53c579517ed324ced5
sbt-velikokhatko-nn <NNVelikokhatko.SBT@sberbank.ru>
2020-11-22 18:24:57 +0300

    29

--- sql_learn_pg/29.sql
+++ sql_learn_pg/29.sql
@@ -1,7 +1,7 @@
 select income_o.point,
        income_o.date,
-       case when income_o.inc is null then 'NULL' else cast(income_o.inc AS varchar) end   income,
-       case when outcome_o.out is null then 'NULL' else cast(outcome_o.out AS varchar) end outcome
+       income_o.inc  income,
+       outcome_o.out outcome
 from income_o
          left join outcome_o on income_o.point = outcome_o.point and income_o.date = outcome_o.date
 
@@ -9,7 +9,7 @@ union
 
 select outcome_o.point,
        outcome_o.date,
-       case when income_o.inc is null then 'NULL' else cast(income_o.inc AS varchar) end   income,
-       case when outcome_o.out is null then 'NULL' else cast(outcome_o.out AS varchar) end outcome
+       income_o.inc  income,
+       outcome_o.out outcome
 from income_o
          right join outcome_o on income_o.point = outcome_o.point and income_o.date = outcome_o.date
