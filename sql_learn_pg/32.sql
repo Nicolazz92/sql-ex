c37b12977bb33c69a29e29000a95e71a029b539b
sbt-velikokhatko-nn <NNVelikokhatko.SBT@sberbank.ru>
2020-11-30 16:09:51 +0300

    32

---
+++ sql_learn_pg/32.sql
@@ -0,0 +1,13 @@
+select distinct country, cast(avg(power(bor, 3) / 2) as numeric(6,2)) as mw
+from (
+         select country, name, bore bor
+         from ships
+                  inner join classes on classes.class = ships.class
+
+         union
+
+         select country, ship, bore bor
+         from outcomes
+                  inner join classes on classes.class = outcomes.ship
+     ) as bores
+group by country;
