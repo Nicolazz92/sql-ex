c7e017b6862942a6de5c57549ee8d6068a3cecef
sbt-velikokhatko-nn <NNVelikokhatko.SBT@sberbank.ru>
2021-01-11 18:34:01 +0300

    text

--- sql_learn_pg/68.sql
+++ sql_learn_pg/68.sql
@@ -1,7 +1,7 @@
-with trip_count as (select hashtext(town_from) / 10000 + hashtext(town_to) / 10000 hash, count(trip_no) _count
+with trip_count as (select greatest(town_from, town_to) || least(town_from, town_to) appended, count(trip_no) _count
                     from trip
-                    group by hash)
+                    group by appended)
 
 select count(1)
 from trip_count tc
-where tc._count >= all (select _count from trip_count)
+where tc._count >= all (select _count from trip_count);
