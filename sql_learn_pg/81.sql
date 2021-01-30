ad11bfb0a510fb702d2d284de74cef00f4ca2f43
Nicolazz92 <gurzufina@yandex.ru>
2021-01-30 15:41:12 +0300

    text

--- sql_learn_pg/81.sql
+++ sql_learn_pg/81.sql
@@ -1,10 +1,10 @@
 -- Из таблицы Outcome получить все записи за тот месяц (месяцы), с учетом года, в котором суммарное значение расхода (out) было максимальным.
 
-with max_out_year_month as (select sum(out) _sum, date_trunc('year', date) _year, date_trunc('month', date) _month
+with out_per_year_month as (select sum(out) _sum, date_trunc('year', date) _year, date_trunc('month', date) _month
                             from outcome
                             group by _year, _month
-                            order by _sum desc
-                            limit 1)
+                            order by _sum desc),
+     max_out_year_month as (select * from out_per_year_month where _sum = (select max(_sum) from out_per_year_month))
 
 select code, point, date, out
 from outcome
